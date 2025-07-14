316dafe6e9445aab538aefaa260c3a299fa3467c
niko <reowqwerwerweafkdjhsf@mail.ru>
2025-07-14 04:23:10 +0300

    initial commit

---
+++ eslint.config.js
@@ -0,0 +1,49 @@
+// eslint.config.js
+import js from '@eslint/js';
+import globals from 'globals';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import jsxA11y from 'eslint-plugin-jsx-a11y';
+import reactRefresh from 'eslint-plugin-react-refresh';
+
+export default [
+  {
+    ignores: ['dist', 'node_modules']
+  },
+  {
+    files: ['**/*.{js,jsx}'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      parserOptions: {
+        ecmaFeatures: { jsx: true }
+      },
+      globals: {
+        ...globals.browser
+      }
+    },
+    plugins: {
+      react,
+      'react-hooks': reactHooks,
+      'jsx-a11y': jsxA11y,
+      'react-refresh': reactRefresh
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+      ...react.configs.recommended.rules,
+      ...reactHooks.configs.recommended.rules,
+      'react/react-in-jsx-scope': 'off',
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+      'jsx-a11y/anchor-is-valid': ['error', {
+        'components': ['Link'],
+        'specialLink': ['hrefLeft', 'hrefRight'],
+        'aspects': ['invalidHref', 'preferButton']
+      }],
+      'semi': ['error', 'always', { 'omitLastInOneLineBlock': false }],  // если точка с запятой пропущена, показывает ошибку
+      'comma-dangle': ['error', 'never'],  // считать ли ошибкой висячую запятую. never - никогда не допускать их, always - всегда висячая запятая должна стоять
+      'quotes': ['error', 'single'],  // форматирование кавычек, все в single
+      'react/prop-types': 0,  // отключает пропсы
+      'indent': ['error', 2]  // 2 пробела
+    }
+  }
+];
